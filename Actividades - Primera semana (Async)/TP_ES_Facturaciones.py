37795df693abbde5471259d9db6f554111cc28bd
luvlsco <109562365+luvlsco@users.noreply.github.com>
2024-07-08 16:49:32 -0300

    ...

--- Actividades - Primera semana (Async)/TP_ES_Facturaciones.py
+++ Actividades - Primera semana (Async)/TP_ES_Facturaciones.py
@@ -1,25 +1,25 @@
-# A) Ingresar tres precios de productos y mostrar la suma de los mismos.
-producto_1 = int(input("Ingresa el precio del primer producto: "))
-producto_2 = int(input("Ingresa el precio del segundo producto: "))
-producto_3 = int(input("Ingresa el precio del tercer producto: "))
+# A) Ingresár tres precios de productos y mostrar la suma de los mismos.
+producto_1 = int(input("Ingresá el precio del primer producto: "))
+producto_2 = int(input("Ingresá el precio del segundo producto: "))
+producto_3 = int(input("Ingresá el precio del tercer producto: "))
 
 total = producto_1 + producto_2 + producto_3
 
 print("\n=== PRECIOS ===","\nPrecio del primer producto:",producto_1,"\nPrecio del segundo producto:",producto_2,"\nPrecio del tercer producto:",producto_3,"\n\n=== TOTAL ===","\nSuma de los precios:",total,"\n",sep=" ")
 
-# B) Ingresar tres precios de productos y mostrar el promedio de los mismos.
-producto_1 = int(input("Ingresa el precio del primer producto: "))
-producto_2 = int(input("Ingresa el precio del segundo producto: "))
-producto_3 = int(input("Ingresa el precio del tercer producto: "))
+# B) Ingresár tres precios de productos y mostrar el promedio de los mismos.
+producto_1 = int(input("Ingresá el precio del primer producto: "))
+producto_2 = int(input("Ingresá el precio del segundo producto: "))
+producto_3 = int(input("Ingresá el precio del tercer producto: "))
 
 promedio = (producto_1 + producto_2 + producto_3) / 3
 
 print("\n=== PRECIOS ===","\nPrecio del primer producto:",producto_1,"\nPrecio del segundo producto:",producto_2,"\nPrecio del tercer producto:",producto_3,"\n\n=== PROMEDIO ===","\nPromedio de los precios:",promedio,"\n",sep=" ")
 
 # C) ingresar tres precios de productos sumarlos y mostrar el precio final (más IVA 21%).
-producto_1 = int(input("Ingresa el precio del primer producto: "))
-producto_2 = int(input("Ingresa el precio del segundo producto: "))
-producto_3 = int(input("Ingresa el precio del tercer producto: "))
+producto_1 = int(input("Ingresá el precio del primer producto: "))
+producto_2 = int(input("Ingresá el precio del segundo producto: "))
+producto_3 = int(input("Ingresá el precio del tercer producto: "))
 
 iva = (producto_1 + producto_2 + producto_3) * 1.21
 
