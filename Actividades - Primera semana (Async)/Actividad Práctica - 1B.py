db2cdaed5e282f0f94eda31dfedc595c0a1e6b27
Lucas Velasco <109562365+luvlsco@users.noreply.github.com>
2024-07-20 20:48:43 -0300

    ...

--- Actividades - Primera semana (Async)/Actividad Práctica - 1B.py
+++ Actividades - Primera semana (Async)/Actividad Práctica - 1B.py
@@ -1,15 +1,11 @@
 #Actividad Práctica - 1B - Python Unidad 1
 # 1) Escribe un programa que pida el nombre del usuario y muestre por consola 
 # un texto: “Hola nombreUsuario”
-print("\n 1) Programa que pida el nombre del usuario y muestre por consola 'Hola <usuario>'\n")
-
 nombre_usuario = input("Ingresá tu nombre de usuario: ")
 print("Hola",nombre_usuario,sep=" ")
 
 # 2) Escribe un programa que pida un número, luego pida otro número y
 # muestre por consola el resultado de sumar dichos números.
-print("\n 2) Programa que pida un número, luego pida otro número y muestre por consola el resultado de sumar dichos números\n")
-
 num_1 = int(input("Ingresá un número: "))
 num_2 = int(input("Ingresá el siguiente número: "))
 resultado = num_1 + num_2
@@ -18,8 +14,6 @@ print("Resultado:",resultado,sep=" ")
 
 # 3) Crear un programa que pida el nombre, el apellido, edad del usuario y luego
 # muestre por consola los datos ingresados
-print("\n 3) Programa que pida el nombre, el apellido, edad del usuario y luego muestre por consola los datos ingresados\n")
-
 nombre = input("Ingresá tu nombre: ")
 apellido = input("Ahora ingresa tu apellido: ")
 edad = int(input("Por último, ingresa tu edad: "))
@@ -29,8 +23,6 @@ print("Hola",nombre,apellido,"tenés",edad,"años.",sep=" ")
 # 4) Crear un programa que pida el nombre, número de comisión, asignatura,
 # docente y si el usuario estuvo presente, luego que muestre los datos por
 # consola con las leyendas pertinentes.
-print("\n 4) Programa que pida el nombre, número de comisión, asignatura, docente y si el usuario estuvo presente, luego que muestre los datos por consola\n")
-
 nombre = input("Ingresá tu nombre: ")
 num_comision = int(input("Ingresá el número de tu comisión: "))
 asignatura = input("Ingresá la asignatura: ")
@@ -41,16 +33,12 @@ print("=== DATOS ===")
 print("\nNombre:",nombre,"\nComisión:",num_comision,"\nAsignatura:",asignatura,"\nDocente:",docente,"\nPresente:",presente,sep=" ")
 
 # 5) Crear un programa que pida el lado de un cuadrado y calcule la superficie.
-print("\n 5) Programa que pida el lado de un cuadrado y calcule la superficie\n")
-
 lado = int(input("Ingresá la longitud del lado del cuadrado: "))
 resultado = lado ** 2
 
 print("La superficie del cuadrado es de",resultado,sep=" ")
 
 # 6) Crear un programa que pida los lados de un rectángulo y calcule la superficie.
-print("\n 6) Programa que pida los lados de un rectángulo y calcule la superficie\n")
-
 base = int(input("Ingresá la longitud de la base del rectángulo: "))
 altura = int(input("Ingresá la longitud de la altura del rectángulo: "))
 resultado = (base * altura)
@@ -58,8 +46,6 @@ resultado = (base * altura)
 print("La superficie del rectángulo es de",resultado,sep=" ")
 
 # 7) Crear un programa que calcule la superficie de un triángulo.
-print("\n 7) Programa que calcule la superficie de un triángulo\n")
-
 base = int(input("Ingresá la longitud de la base del triángulo: "))
 altura = int(input("Ingresá la longitud de la altura del triángulo: "))
 resultado = (base * altura) / 2
@@ -67,8 +53,6 @@ resultado = (base * altura) / 2
 print("La superficie del triángulo es de",resultado,sep=" ")
 
 # 8) Crear un programa que permite ingresar el nombre de un producto, el precio y que calcule el IVA.
-print("\n 8) Programa que permite ingresar el nombre de un producto, el precio y que calcule el IVA.\n")
-
 producto = input("Ingresá el nombre del producto: ")
 precio = float(input("Ingresá el precio del producto: "))
 iva = precio * 1.21
@@ -77,8 +61,6 @@ print("El precio final del producto es:",iva,sep=" ")
 
 # 9) Crear un programa que calcule el promedio de un alumno, ingresando su
 # nombre apellido, 3 notas, que muestre al final las leyendas pertinentes.
-print("\n 9) Programa que calcule el promedio de un alumno, ingresando su nombre apellido, 3 notas, que muestre al final las leyendas pertinentes.\n")
-
 nombre_apellido = input("Ingresá el nombre y apellido del alumno: ")
 calificacion_1 = float(input("Ingresá la primera calificación del alumno: "))
 calificacion_2 = float(input("Ingresá la segunda calificación del alumno: "))
@@ -89,8 +71,6 @@ print("La calificación promedio del alumno",nombre_apellido,"es de",promedio,se
 
 # 10) Crear un programa en el cual pida al usuario el nombre y la edad y muestre
 # por consola la edad que tendra dentro de 10 años.
-print("\n 10) Programa el cual pida al usuario el nombre y la edad y muestre por consola la edad que tendra dentro de 10 años.\n")
-
 nombre = input("Ingresá tu nombre: ")
 edad = int(input("Ingresá tu edad: "))
 futuro = edad + 10
